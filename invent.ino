925fa172d0d68cc399e08bc70ce485f5b192ffad
Rich Jefferson <rrjefferson@gmail.com>
2021-11-28 21:57:19 -0500

    Removing delays and interrupts.

--- invent.ino
+++ invent.ino
@@ -3,40 +3,63 @@
 
 const int MAX_SPRAYS = 10;      // Number of sprays when refilled
 
-const byte RESET_INT_PIN = 2;   // Pin for INT.0 to get reset button
-const byte DISPLAY_INT_PIN = 3; // Pin for INT.1 to get display button
+const byte RESET_PIN = 2;   // Pin for reset button
+const byte DISPLAY_PIN = 3; // Pin for display button
 
 const byte SPRAY_RELAY_PIN = 7; // Pin to control power relay to spray
 
-const unsigned long PRE_SPRAY_TIME     = 1000UL * 60; // ms * seconds = 1 minute
-const unsigned long LONG_DISPLAY_TIME  = 1000UL * 60; // ms * seconds = 1 minute
-const unsigned long SHORT_DISPLAY_TIME = 1000UL * 10; // ms * seconds = 10s
-const unsigned long SPRAY_TIME         = 1000UL * 30; // ms * seconds = 30s
-//const unsigned long SPRAY_WAIT         = 1000UL * 14 * 24 * 60 * 60; // ms * days * hours * minutes * seconds = 2 weeks
-const unsigned long SPRAY_WAIT         = 1000UL * 30;
-const unsigned long NO_SPRAY_WAIT      = 1000UL * 30;
+const unsigned long DEBOUNCE_DELAY = 50UL; // Value to filture button bounces
 
-volatile int spraysRemaining;   // Number of sprays remaining - may be changed by interrupts so volatile
+const unsigned long PRE_SPRAY_TIME     = 1000UL * 60; // ms * seconds = 1 minute
+const unsigned long POST_SPRAY_TIME    = 1000UL * 60; // ms * seconds = 1 minute
+const unsigned long SPRAY_TIME         = 1000UL * 30; // ms * seconds = 30s
+const unsigned long SHORT_DISPLAY_TIME = 1000UL * 10; // ms * seconds = 10s
+//const unsigned long SPRAY_TIME         = 1000UL * 14 * 24 * 60 * 60; // ms * days * hours * minutes * seconds = 2 weeks
+const unsigned long SPRAY_WAIT_TIME    = 1000UL * 30;
+const unsigned long NO_SPRAY_WAIT_TIME = 1000UL * 30;
+
+// Loop state definitions and current loop state variable
+const int PRE_SPRAY_STATE = 0;
+const int SPRAYING_STATE = 1;
+const int POST_SPRAY_STATE = 2;
+const int WAITING_STATE = 3;
+
+int currentState;
+
+// Variables for keeping track of button presses and debouncing them
+int lastResetButtonState = LOW;
+int lastDisplayButtonState = LOW;
+unsigned long lastResetButtonDebounceTime = 0UL;
+unsigned long lastDisplayButtonDebounceTime = 0UL;
+
+// Time variables for tracking passage of time without using delay()
+unsigned long lastLoopMillis;
+long remainingStateMillis;
+
+int spraysRemaining;   // Number of sprays remaining
+
+// Variable for keeping track of display state
+bool displayOn = false;
+long remainingDisplayTime = 0;
 
 // Initialize the LCD library with the numbers of the interface pins
 LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 
 void setup() {
+    Serial.begin(115200);
+
     // Assume the tank is full and set spray count
     spraysRemaining = MAX_SPRAYS;
 
+    // We'll always start in the pre-spray state
+    currentState = PRE_SPRAY_STATE;
+
     // Set our button pins to be input mode
-    pinMode(RESET_INT_PIN, INPUT);
-    pinMode(DISPLAY_INT_PIN, INPUT);
+    pinMode(RESET_PIN, INPUT);
+    pinMode(DISPLAY_PIN, INPUT);
 
-    // Attach our interrupt handlers to perform actions on button press
-    // these temporarily interrupt loop() to perform the needed acctions
-    // then return to loop() where it left off -
-    // FALLING means when the button is released after being pressed
-    attachInterrupt(digitalPinToInterrupt(RESET_INT_PIN), doReset, FALLING);
-    attachInterrupt(digitalPinToInterrupt(DISPLAY_INT_PIN), doDisplay, FALLING);
-
-    // Set our spray control pin to be output mode and turn the relay off
+    // Set our spray control pin to be output mode
+    // and turn the relay off by setting it to low
     pinMode(SPRAY_RELAY_PIN, OUTPUT);
     digitalWrite(SPRAY_RELAY_PIN, LOW);
 
@@ -48,38 +71,105 @@ void setup() {
     lcd.display();
     lcd.setCursor(0,0);
     lcd.print("Setup done");
-    delay(10000);
+
+    // Init time varaiables
+    remainingStateMillis = PRE_SPRAY_TIME;
+    lastLoopMillis = millis();
 }
 
 void loop() {
+    //debug("loop");
 
-    if (spraysRemaining > 0) {
-        // Do spraying process
-        delay(PRE_SPRAY_TIME);
-        turnOnSpray();
-        delay(SPRAY_TIME);
-        turnOffSpray();
-
-        spraysRemaining--; // Reduce spray count by one.
-
-        // Show remaining sprays
-        turnOnDisplay();
-        delay(LONG_DISPLAY_TIME);
-        turnOffDisplay();
-
-        // Wait a long period before spraying again
-        delay(SPRAY_WAIT);
-    } else {
-        // Wait a short period before checking the remaining sprays again
-        delay(NO_SPRAY_WAIT);
+    if (displayOn) {
+        remainingDisplayTime -= (millis() - lastLoopMillis);
+        if (remainingDisplayTime <= 0) {
+            turnOffDisplay();
+            displayOn = false;
+        }
     }
 
+    switch (currentState) {
+        case PRE_SPRAY_STATE:
+            preSprayState();
+            break;
+        case SPRAYING_STATE:
+            sprayingState();
+            break;
+        case POST_SPRAY_STATE:
+            postSprayState();
+            break;
+        case WAITING_STATE:
+            waitingState();
+            break;
+        default:
+            Serial.print("Unexpected state");
+    }
+
+    doCheckButtons();
+
+    lastLoopMillis = millis();
+}
+
+void preSprayState() {
+    debug("prespray");
+    // Reduce remaining time by the delta used by the last loop
+    remainingStateMillis -= (millis() - lastLoopMillis);
+    if (remainingStateMillis <= 0) {
+        // Transition to next state if this state's time has expired
+        currentState = SPRAYING_STATE;
+        remainingStateMillis = SPRAY_TIME;
+    }
+}
+
+void sprayingState() {
+    debug("spraying");
+    // See if this is the first exeuction
+    if (remainingStateMillis == SPRAY_TIME) {
+        turnOnSpray();
+    }
+    remainingStateMillis -= (millis() - lastLoopMillis);
+    if (remainingStateMillis <= 0) {
+        // Transition to next state as this state's time has expired
+        turnOffSpray();
+        currentState = POST_SPRAY_STATE;
+        remainingStateMillis = POST_SPRAY_TIME;
+    }
+}
+
+void postSprayState() {
+    debug("postspray");
+    //See if this is the first execution
+    if (remainingStateMillis == POST_SPRAY_TIME) {
+        spraysRemaining--;
+        turnOnDisplay();
+    }
+    remainingStateMillis -= (millis() - lastLoopMillis);
+    if (remainingStateMillis <= 0) {
+        // Transition to next state as this state's time has expired
+        turnOffDisplay();
+        currentState = WAITING_STATE;
+        remainingStateMillis = (spraysRemaining > 0 ? SPRAY_WAIT_TIME : NO_SPRAY_WAIT_TIME);
+    }
+}
+
+void waitingState() {
+    debug("waiting");
+    remainingStateMillis -= (millis() - lastLoopMillis);
+    if (remainingStateMillis <= 0) {
+        // Transition to next state if there are sprays left or stay in this one if there are not
+        if (spraysRemaining > 0) {
+            currentState = PRE_SPRAY_STATE;
+            remainingStateMillis = PRE_SPRAY_TIME;
+        } else {
+            remainingStateMillis = NO_SPRAY_WAIT_TIME;
+        }
+    }
 }
 
 void turnOnSpray() {
     lcd.clear();
     lcd.display();
-    lcd.print("SPRAYING SPRAYING");
+    lcd.print("SPRAYING NOW");
     digitalWrite(SPRAY_RELAY_PIN, HIGH); // Turn on the pin to allow power to flow
     // through the relay to the spray pump
 }
@@ -112,14 +202,67 @@ void turnOffDisplay() {
 }
 
 void doDisplay() {
-    // Turn on the display, wait a short time and then turn it off
+    // Turn on the display, and setup the display state to wait
     turnOnDisplay();
-    delay(SHORT_DISPLAY_TIME);
-    turnOffDisplay();
+    remainingDisplayTime = SHORT_DISPLAY_TIME;
+    displayOn = true;
 }
 
 void doReset() {
     // Someone has refilled the tank and pressed the reset button,
     // reset the number of sprays to the maximum for the tank.
     spraysRemaining = MAX_SPRAYS;
+}
+
+void doCheckButtons() {
+    // Check the reset button first
+    int resetButtonRead = digitalRead(RESET_PIN);
+
+    // If it changed, reset the debounce time
+    if (resetButtonRead != lastResetButtonState) {
+        lastResetButtonDebounceTime = millis();
+    }
+
+    if ((millis() - lastResetButtonDebounceTime) > DEBOUNCE_DELAY) {
+        // This state has held long enough to be debounced so chage the current state if not
+        // already the same:
+
+        if (resetButtonRead != lastResetButtonState) {
+            lastResetButtonState = resetButtonRead;
+            // If it changed to being pressed, do the reset.
+            if(resetButtonRead = HIGH) {
+                doReset();
+            }
+        }
+    }
+
+    // Now check the display button since that still has a delay in it.. :(
+    int displayButtonRead = digitalRead(DISPLAY_PIN);
+
+    // If it changed, reset the debounce time
+    if (displayButtonRead != lastDisplayButtonState) {
+        lastDisplayButtonDebounceTime = millis();
+    }
+
+    if ((millis() - lastDisplayButtonDebounceTime) > DEBOUNCE_DELAY) {
+        // This state has held long enough to be debounced so chage the current state if not
+        // already the same:
+
+        if (displayButtonRead != lastDisplayButtonState) {
+            lastDisplayButtonState = displayButtonRead;
+            // If it changed to being pressed, do the display.
+            if(displayButtonRead = HIGH) {
+                doDisplay();
+            }
+        }
+    }
+
+}
+
+void debug(char * buffer) {
+    lcd.display();
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(buffer);
 }
